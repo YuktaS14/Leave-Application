e93a90148631e1a11f97b0b4d441f74036189969
dalvimangesh <dalvimangesh000@gmail.com>
2023-04-14 00:06:44 +0530

    login

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require("express")
+const session = require("express-session")
+const passport = require("passport")
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const ejs = require("ejs")
+const path = require("path")
+require("dotenv").config()
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(session({
+    secret: "key", // can be anything
+    resave: false,
+    saveUninitialized: false,
+    // local server has http, if we have https we can set it to true
+    cookie: { secure: false },
+}));
+
+app.use(passport.initialize())
+// first we need to create express session then only we can use this session
+app.use(passport.session())
+
+// two input 1st options, 2nd one function
+passport.use(new GoogleStrategy({
+
+    clientID:
+        "213766690937-p6j2lpceusl9ee7f99he8vsuf4010l37.apps.googleusercontent.com",
+    clientSecret:
+        "",
+    callbackURL:
+        "http://localhost:5000/auth/google/callback",
+    // where we want to go ?
+    // can be any route
+    // redirect and callback are same
+
+}, function (accesstoken, refreshtoken, profile, cb) {
+    // cb = callback function
+    cb(null, profile);
+}));
+
+// to store user in session
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user)
+})
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj)
+})
+
+
+app.get('/login', (req, res) => {
+    res.render("login.ejs")
+})
+
+app.get("/auth/google", passport.authenticate("google", {
+    scope: ["profile", "email"]
+}
+));
+
+
+app.get("/dashboard", (req, res) => {
+    if (req.isAuthenticated()) {
+        console.log(req.user)
+        res.render("dashbord.ejs", {
+            user: req.user
+        });
+    } else {
+        res.redirect("/login");
+    }
+})
+
+// verifying again
+app.get("/auth/google/callback", passport.authenticate("google", {
+    failureRedirect: "/login"
+}), async (req, res) => {
+    res.redirect("/dashboard")
+}
+);
+
+app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            req.session.destroy();
+            res.redirect("/login");
+        }
+    });
+});
+
+
+app.listen(5000, () => {
+    console.log("server working")
+})

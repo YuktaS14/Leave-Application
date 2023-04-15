1e0d736d93bfde879e63cbecde292b5edb43fd83
Yukta <112001052@smail.iitpkd.ac.in>
2023-04-15 18:39:06 +0530

    Admin can add student data, add holdays

--- routes/admin.js
+++ routes/admin.js
@@ -14,11 +14,42 @@ router.get("/",(req,res)=>{
 });
 });
 
-router.get("/addStudent",(req,res)=>{
-    res.render("../views/AddStudent.ejs");
+//why is req empty here :.. 
+router.post("/", async (req,res) =>{
+    console.log(req.body)
 });
 
+router.get("/addStudent",(req,res)=>{
+    res.render("../views/AddStudent.ejs",{message: req.flash('studentInsert')});
+});
+
+
 router.post("/addStudent",(req,res)=>{
+    var rollNo =  req.body.RollNo;
+    var name = req.body.name;
+    var leaves =  req.body.leavesAlloted;
+    var FA = req.body.FA;
+    var PM =  req.body.PM;
+    var insertData = `
+    INSERT INTO studentfaculty (rollno,name,facultyadvisor,projectmentor)
+    VALUES (${rollNo},'${name}','${FA}','${PM}')
+    `;
+
+    var insertData2 = `
+    INSERT INTO studentinfo (rollno,name,leavesleft)
+    VALUES (${rollNo},'${name}',${leaves})
+    `;
+    dbConnect.query(insertData,(err,result)=>{
+    if(err) throw err;
+    else{
+    dbConnect.query(insertData2,(err,result)=>{
+    if(err) throw err;
+    else{
+          req.flash('studentInsert','Added to database successfully!! ');
+          res.redirect("/admin/addStudent")
+    }
+})}
+});
     console.log(req.body);
 });
 

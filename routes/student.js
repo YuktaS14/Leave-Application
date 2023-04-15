6259d042b964920f23eba96e57609d788c0f6bc3
dalvimangesh <dalvimangesh000@gmail.com>
2023-04-15 23:06:18 +0530

    student

--- routes/student.js
+++ routes/student.js
@@ -1,12 +1,37 @@
 const express = require("express");
-
+const { dbConnect } = require("../data/database");
 const router = express.Router();
 
 
 router.get("/",(req,res,next)=>{
-    res.render("studentHomePage.ejs",{
-        "name" : "Mangesh"
-    })
+
+    rollno = 112001010
+
+    let temp = {}
+
+    async function fun() {
+
+    await dbConnect.query(`select * from studentInfo where rollno = ${rollno}`, (err,result)=>{
+        if(!err){
+            console.log(result.rows);
+            temp.name = result.rows;
+        }
+    });
+    }
+
+    fun()
+
+    // dbConnect.query(`select * from leaveapplications where rollno = ${rollno}`,(err,result)=>{
+    //     if(!err){
+    //         // console.log(result.rows);
+    //         result.leaveapplication = result.rows;
+    //     }
+    // });
+    console.log('rishav')
+    console.log(temp)
+    console.log('rishav')
+
+    // res.render("studentHomePage.ejs", obj )
 });
 
 

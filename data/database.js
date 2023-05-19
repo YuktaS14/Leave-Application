eddebf1d52eaaa05bc9f6d9b46305ed14bd2a38b
dalvimangesh <dalvimangesh000@gmail.com>
2023-05-19 21:38:51 +0530

    added date to string

--- data/database.js
+++ data/database.js
@@ -1,6 +1,15 @@
 const pkg  = require("pg");
 const {Client} = pkg;
 
+
+
+pkg.types.setTypeParser(1114, function (value) {
+  return value
+})
+
+pkg.types.setTypeParser(1082, function(value) { 
+    return value;
+});
 const client = new Client({
     host:"localhost",
     timezone: 'Asia/Kolkata',

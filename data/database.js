8b4e6ccf7c7b410ecd02b181e81b0844b8aab363
dalvimangesh <dalvimangesh000@gmail.com>
2023-05-19 21:37:28 +0530

    .

--- data/database.js
+++ data/database.js
@@ -1,15 +1,6 @@
 const pkg  = require("pg");
 const {Client} = pkg;
 
-
-pkg.types.setTypeParser(1114, function (value) {
-  return value
-})
-
-pkg.types.setTypeParser(1082, function(value) { 
-    return value;
-});
-
 const client = new Client({
     host:"localhost",
     timezone: 'Asia/Kolkata',
